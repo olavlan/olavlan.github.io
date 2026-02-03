ab8c9e5e1e5ee43ef9c03f12568083228f30176a
Olav Landsverk <olav.landsverk@gmail.com>
2026-02-03 18:48:22 +0100

    skeleton
    Signed-off-by: Olav Landsverk <olav.landsverk@gmail.com>

--- prototypes/epubwords/src/epubwords/webapp/epub.py
+++ prototypes/epubwords/src/epubwords/webapp/epub.py
@@ -25,41 +25,43 @@ def list_ebooks(
 ) -> str:
     ebook_records = database_client.get_ebook_list()
 
-    add_book_html = """
+    add_ebook_html = """
         <form action="/upload" method="post" enctype="multipart/form-data">
           <input type="file" name="file">
           <button type="submit">Upload</button>
         </form>
     """
-
-    html = "<table>"
-    html += f"<thead><tr><td>{add_book_html}</td></tr></thead>"
-    html += "<tbody>"
+    html = f"""
+        <table>
+            <thead><tr><td>{add_ebook_html}</td></tr></thead>
+            <tbody>
+    """
     for e in ebook_records:
         html += f"""
             <tr>
                 <td><a href="/{e.id}">{e.title}</a></td>
             </tr>
         """
-    html += "</tbody>"
-    html += "</table>"
-
+    html += """
+            </tbody>
+        </table>
+    """
     return html
 
 
-@router.get("/{book_id}", response_class=HTMLResponse)
+@router.get("/{ebook_id}", response_class=HTMLResponse)
 def list_ebook_chapters(
-    book_id: int,
+    ebook_id: int,
     database_client: db.DatabaseClient = Depends(db.get_database_client),
 ) -> str:
-    chapter_records = database_client.get_chapter_list(book_id)
+    chapter_records = database_client.get_chapter_list(ebook_id)
 
     html = "<table>"
     html += "<tbody>"
     for c in chapter_records:
         html += f"""
             <tr>
-                <td><a href="/{book_id}/{c.number}">{c.title}</a></td>
+                <td><a href="/{ebook_id}/{c.number}">{c.title}</a></td>
             </tr>
         """
     html += "</tbody>"
@@ -68,19 +70,21 @@ def list_ebook_chapters(
     return html
 
 
-@router.get("/{book_id}/{chapter_number}", response_class=HTMLResponse)
+@router.get("/{ebook_id}/{chapter_number}", response_class=HTMLResponse)
 def show_chapter_words(
-    book_id: str,
+    ebook_id: int,
     chapter_number: int,
     database_client: db.DatabaseClient = Depends(db.get_database_client),
 ) -> str:
     chapter = database_client.get_chapter(ebook_id, chapter_number)
+    # extract words from chapter
+    words = []
     html = "<table>"
     html += "<tbody>"
     for word in words:
         html += f"""
             <tr>
-                <td><a href="/{book_id}/{chapter_number}/{word}">{word}</a></td>
+                <td><a href="/{ebook_id}/{chapter_number}/{word}">{word}</a></td>
             </tr>
         """
     html += "</tbody>"
